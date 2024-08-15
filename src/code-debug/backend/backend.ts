70c41fe8879780000bb2f9d1d43fdce95323a73d
Nikodem Rabuliński <nrabulinski@antmicro.com>
2024-08-15 12:01:58 +0200

    Add debugging capability using GDB and MI2 protocol

---
+++ src/code-debug/backend/backend.ts
@@ -0,0 +1,169 @@
+// Copyright (c) 2024 Antmicro <www.antmicro.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// This code is based on https://github.com/WebFreak001/code-debug/blob/f204573fae2f9614aba4be3307984c28576f3b8d/src/backend/backend.ts
+
+import { MINode } from './mi_parse';
+import { DebugProtocol } from '@vscode/debugprotocol/lib/debugProtocol';
+
+export type ValuesFormattingMode = 'disabled' | 'parseText' | 'prettyPrinters';
+
+export interface Breakpoint {
+  id?: number;
+  file?: string;
+  line?: number;
+  raw?: string;
+  condition?: string;
+  countCondition?: string;
+  logMessage?: string;
+}
+
+export interface Thread {
+  id: number;
+  targetId: string;
+  name?: string;
+}
+
+export interface Stack {
+  level: number;
+  address: string;
+  function: string;
+  fileName: string;
+  file: string;
+  line: number;
+}
+
+export interface Variable {
+  name: string;
+  valueStr: string;
+  type: string;
+  raw?: any;
+}
+
+export interface RegisterValue {
+  index: number;
+  value: string;
+}
+
+export interface IBackend {
+  start(runToStart: boolean): Thenable<boolean>;
+  stop(): void;
+  detach(): void;
+  interrupt(): Thenable<boolean>;
+  continue(): Thenable<boolean>;
+  next(): Thenable<boolean>;
+  step(): Thenable<boolean>;
+  stepOut(): Thenable<boolean>;
+  loadBreakPoints(
+    breakpoints: Breakpoint[],
+  ): Thenable<[boolean, Breakpoint?][]>;
+  addBreakPoint(breakpoint: Breakpoint): Thenable<[boolean, Breakpoint?]>;
+  removeBreakPoint(breakpoint: Breakpoint): Thenable<boolean>;
+  clearBreakPoints(source?: string): Thenable<any>;
+  getThreads(): Thenable<Thread[]>;
+  getStack(
+    startFrame: number,
+    maxLevels: number,
+    thread: number,
+  ): Thenable<Stack[]>;
+  getStackVariables(thread: number, frame: number): Thenable<Variable[]>;
+  evalExpression(name: string, thread: number, frame: number): Thenable<any>;
+  isReady(): boolean;
+  changeVariable(name: string, rawValue: string): Thenable<any>;
+  examineMemory(from: number, to: number): Thenable<any>;
+}
+
+export class VariableObject {
+  name: string;
+  exp: string;
+  numchild: number;
+  type: string;
+  value: string;
+  threadId: string;
+  frozen: boolean;
+  dynamic: boolean;
+  displayhint: string;
+  hasMore: boolean;
+  id: number = 0;
+  constructor(node: any) {
+    this.name = MINode.valueOf(node, 'name');
+    this.exp = MINode.valueOf(node, 'exp');
+    this.numchild = parseInt(MINode.valueOf(node, 'numchild'));
+    this.type = MINode.valueOf(node, 'type');
+    this.value = MINode.valueOf(node, 'value');
+    this.threadId = MINode.valueOf(node, 'thread-id');
+    this.frozen = !!MINode.valueOf(node, 'frozen');
+    this.dynamic = !!MINode.valueOf(node, 'dynamic');
+    this.displayhint = MINode.valueOf(node, 'displayhint');
+    // TODO: use has_more when it's > 0
+    this.hasMore = !!MINode.valueOf(node, 'has_more');
+  }
+
+  public applyChanges(node: MINode) {
+    this.value = MINode.valueOf(node, 'value');
+    if (MINode.valueOf(node, 'type_changed')) {
+      this.type = MINode.valueOf(node, 'new_type');
+    }
+    this.dynamic = !!MINode.valueOf(node, 'dynamic');
+    this.displayhint = MINode.valueOf(node, 'displayhint');
+    this.hasMore = !!MINode.valueOf(node, 'has_more');
+  }
+
+  public isCompound(): boolean {
+    return (
+      this.numchild > 0 ||
+      this.value === '{...}' ||
+      (this.dynamic &&
+        (this.displayhint === 'array' || this.displayhint === 'map'))
+    );
+  }
+
+  public toProtocolVariable(): DebugProtocol.Variable {
+    const res: DebugProtocol.Variable = {
+      name: this.exp,
+      evaluateName: this.name,
+      value: this.value === void 0 ? '<unknown>' : this.value,
+      type: this.type,
+      variablesReference: this.id,
+    };
+    return res;
+  }
+}
+
+// from https://gist.github.com/justmoon/15511f92e5216fa2624b#gistcomment-1928632
+export interface MIError extends Error {
+  readonly name: string;
+  readonly message: string;
+  readonly source: string;
+}
+export interface MIErrorConstructor {
+  new (message: string, source: string): MIError;
+  readonly prototype: MIError;
+}
+
+export const MIError: MIErrorConstructor = class MIError {
+  private readonly _message: string;
+  private readonly _source: string;
+  public constructor(message: string, source: string) {
+    this._message = message;
+    this._source = source;
+    Error.captureStackTrace(this, this.constructor);
+  }
+
+  get name() {
+    return this.constructor.name;
+  }
+  get message() {
+    return this._message;
+  }
+  get source() {
+    return this._source;
+  }
+
+  public toString() {
+    return `${this.message} (from ${this._source})`;
+  }
+};
+Object.setPrototypeOf(MIError as any, Object.create(Error.prototype));
+MIError.prototype.constructor = MIError;
---
+++ src/program/gdb.ts
@@ -0,0 +1,275 @@
+// Copyright (c) 2024 Antmicro <www.antmicro.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { DebugProtocol } from '@vscode/debugprotocol';
+import { MI2DebugSession, RunCommand } from '../code-debug/mibase';
+import { MI2 } from '../code-debug/backend/mi2/mi2';
+import * as vscode from 'vscode';
+import { RenodeWebSocketPseudoTerminal } from '../console';
+import { RenodePluginContext } from '../context';
+import { URL } from 'url';
+import path from 'path';
+
+function randomPort(): number {
+  const min = 10_000;
+  const max = 11_000;
+
+  const d = max - min + 1;
+
+  let r = Math.random() * d + min;
+  return Math.floor(r);
+}
+
+export interface LaunchRequestArguments
+  extends DebugProtocol.LaunchRequestArguments {
+  resc?: string;
+  repl?: string;
+  elf: string;
+  cwd: string;
+  gdb?: string;
+  pathMappings?: object;
+  terminals?: string[];
+  extraRenodeArgs?: string[];
+  cpuCluster?: string;
+  // TODO: Work on autodetection
+  remoteHypervisor?: boolean;
+}
+
+export class RenodeGdbDebugSession extends MI2DebugSession {
+  private output?: vscode.OutputChannel;
+  private mappings: [string, string][] = [];
+  private terminals: vscode.Terminal[] = [];
+  private renodeStarted = false;
+
+  constructor(private pluginCtx: RenodePluginContext) {
+    super(false, false);
+  }
+
+  protected override initializeRequest(
+    response: DebugProtocol.InitializeResponse,
+    _args: DebugProtocol.InitializeRequestArguments,
+  ): void {
+    response.body ??= {};
+    response.body.supportsDisassembleRequest = true;
+    response.body.supportsReadMemoryRequest = true;
+    response.body.supportsGotoTargetsRequest = true;
+    response.body.supportsTerminateRequest = true;
+    response.body.supportsHitConditionalBreakpoints = true;
+    response.body.supportsConfigurationDoneRequest = true;
+    response.body.supportsConditionalBreakpoints = true;
+    response.body.supportsFunctionBreakpoints = true;
+    // response.body.supportsEvaluateForHovers = true;
+    response.body.supportsSetVariable = true;
+    // response.body.supportsStepBack = true;
+    response.body.supportsLogPoints = true;
+    // response.body.supportsInstructionBreakpoints = true;
+    // response.body.supportsSteppingGranularity = true;
+    this.sendResponse(response);
+  }
+
+  protected setVariableRequest(
+    response: DebugProtocol.SetVariableResponse,
+    args: DebugProtocol.SetVariableArguments,
+  ): Promise<void> {
+    // TODO: Fix setting registers
+    return super.setVariableRequest(response, args);
+  }
+
+  private async launchRequestInner(args: LaunchRequestArguments) {
+    if (this.pluginCtx.isDebugging) {
+      throw new Error('Only one debugging session is supported');
+    }
+
+    const isRemote = args.remoteHypervisor ?? false;
+
+    this.pluginCtx.isDebugging = true;
+
+    let renodeArgs = args.extraRenodeArgs ?? [];
+
+    if (args.resc) {
+      let resc = args.resc;
+      if (isRemote) {
+        const resp = await this.pluginCtx.sendFile(args.resc);
+        resc = resp.path;
+      } else if (!path.isAbsolute(resc)) {
+        resc = path.join(args.cwd, resc);
+      }
+      renodeArgs = [...renodeArgs, '-e', `i @${resc}`];
+    }
+
+    if (args.repl) {
+      let repl = args.repl;
+      if (isRemote) {
+        const resp = await this.pluginCtx.sendFile(args.repl);
+        repl = resp.path;
+      }
+    }
+
+    const gdbPort = randomPort();
+    renodeArgs = [
+      ...renodeArgs,
+      '-e',
+      `machine StartGdbServer ${gdbPort} True ${JSON.stringify(args.cpuCluster ?? 'all')}`,
+    ];
+
+    await this.pluginCtx.startRenode(renodeArgs).catch(() => {
+      throw new Error('Renode did not start');
+    });
+
+    this.renodeStarted = true;
+    vscode.window.showInformationMessage('Renode started');
+    // TODO: Detect when Renode has started instead of always waiting 3s
+    await new Promise(r => setTimeout(r, 3000));
+    const gdbPath = 'gdb';
+    this.mappings = Object.entries(args.pathMappings ?? {});
+    this.miDebugger = new MI2(gdbPath, ['-q', '--interpreter=mi2'], [], null);
+    this.initDebugger();
+    this.setValuesFormattingMode('prettyPrinters');
+    this.initialRunCommand = RunCommand.NONE;
+    const wsUri = new URL(
+      `/run/${args.gdb ?? 'gdb'}`,
+      this.pluginCtx.sessionBase,
+    );
+
+    let elf = args.elf;
+    if (isRemote) {
+      const resp = await this.pluginCtx.sendFile(args.elf);
+      elf = resp.path;
+    } else if (!path.isAbsolute(elf)) {
+      elf = path.join(args.cwd, elf);
+    }
+
+    await this.miDebugger
+      .connectWs(args.cwd, elf, `:${gdbPort}`, wsUri.toString())
+      .catch(err => {
+        throw new Error(`Failed to load debugger: ${err}`);
+      });
+    this.terminals = (args.terminals ?? []).map((terminal, i) => {
+      const name = `Terminal ${i}`;
+      const res = vscode.window.createTerminal({
+        name,
+        pty: new RenodeWebSocketPseudoTerminal(name, terminal),
+      });
+      res.show(false);
+      return res;
+    });
+  }
+
+  protected override async launchRequest(
+    response: DebugProtocol.LaunchResponse,
+    args: LaunchRequestArguments,
+  ) {
+    try {
+      await this.launchRequestInner(args);
+      this.sendResponse(response);
+    } catch (e: any) {
+      let err = e.message ?? e.toString();
+      vscode.window.showErrorMessage(err);
+      this.sendErrorResponse(response, 103, err);
+      this.pluginCtx.isDebugging = false;
+    }
+  }
+
+  protected override terminateRequest(
+    response: DebugProtocol.TerminateResponse,
+    args: DebugProtocol.TerminateArguments,
+  ): Promise<void> {
+    return this.disconnectRequest(response, args);
+  }
+
+  protected override async disconnectRequest(
+    response: DebugProtocol.DisconnectResponse,
+    _args: DebugProtocol.DisconnectArguments,
+  ): Promise<void> {
+    for (const terminal of this.terminals) {
+      terminal.dispose();
+    }
+    this.output?.dispose();
+
+    this.miDebugger?.detach();
+
+    if (this.renodeStarted) {
+      await this.pluginCtx
+        .stopRenode()
+        .then(() => {
+          vscode.window.showInformationMessage('Renode stopped');
+        })
+        .catch(() => {
+          vscode.window.showErrorMessage('Renode did not stop');
+        });
+    }
+    this.sendResponse(response);
+    this.renodeStarted = false;
+    this.pluginCtx.isDebugging = false;
+  }
+
+  protected override async disassembleRequest(
+    response: DebugProtocol.DisassembleResponse,
+    args: DebugProtocol.DisassembleArguments,
+  ): Promise<void> {
+    console.log('[AAA]', JSON.stringify(args));
+
+    const instructions = await this.miDebugger!.disassemble(
+      args.memoryReference,
+    );
+
+    for (let insn of instructions) {
+      if (insn.location && insn.location.path) {
+        insn.location.path = this.convertDebuggerPathToClient(
+          insn.location.path,
+        );
+      }
+    }
+
+    response.body = {
+      instructions,
+    };
+
+    this.sendResponse(response);
+  }
+
+  protected override readMemoryRequest(
+    response: DebugProtocol.ReadMemoryResponse,
+    args: DebugProtocol.ReadMemoryArguments,
+  ): void {
+    let base = parseInt(args.memoryReference) + (args.offset ?? 0);
+    this.miDebugger?.examineMemory(base, args.count).then(
+      data => {
+        console.log('memory data', data);
+        const buf = Buffer.from(data, 'hex');
+        const b64 = buf.toString('base64');
+        response.body ??= { address: base.toString() };
+        response.body.data = b64;
+        this.sendResponse(response);
+      },
+      err => {
+        this.sendErrorResponse(response, 114, `Read memory error: ${err}`);
+      },
+    );
+  }
+
+  protected override convertClientPathToDebugger(clientPath: string): string {
+    let debuggerPath = clientPath;
+    for (const [their, our] of this.mappings) {
+      if (clientPath.startsWith(our)) {
+        return their + clientPath.slice(our.length);
+      }
+    }
+    return debuggerPath;
+  }
+
+  protected override convertDebuggerPathToClient(debuggerPath: string): string {
+    let clientPath = debuggerPath;
+    for (const [their, our] of this.mappings) {
+      if (debuggerPath.startsWith(their)) {
+        return our + debuggerPath.slice(their.length);
+      }
+    }
+    return clientPath;
+  }
+
+  override sendResponse(response: DebugProtocol.Response): void {
+    super.sendResponse(response);
+  }
+}
